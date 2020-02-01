18c45ccd6c961a3538f3dbb11915be27b0848aa8
leye195 <leye195@naver.com>
2020-02-02 00:42:09 +0900

    error fix

--- src/assets/js/videoPlayer.js
+++ src/assets/js/videoPlayer.js
@@ -98,7 +98,7 @@ const formatTime = seconds => {
   }
   return `${hours}:${minutes}:${totalSeconds}`;
 };
-const setTotalTime = async () => {
+const setTotalTime = () => {
   /*const blob = await axios({
     url: videoPlayer.src,
     method: "get",
@@ -107,7 +107,7 @@ const setTotalTime = async () => {
   //const blob = await fetch(videoPlayer.src).then(response => response.blob());
   //duration = await getBlobDuration(blob);
   // console.log(duration + " seconds");
-  const total = await formatTime(videoPlayer.duration);
+  const total = formatTime(videoPlayer.duration);
   console.log(total);
   totalTime.innerHTML = total;
 };
